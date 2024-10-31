5cc0f36d3292b70aa803a7727212d88f8ae39b1a
mpio <mpiotro4@outlook.com>
2024-10-31 13:15:23 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Upewnij się, że używasz silnego klucza
+
+translations = {
+    'pl': {
+        'title': 'Marcin Piotrowski - strona domowa',
+        'about_me': 'O mnie',
+        'about_me_content': 'Posiadam ponad 3 lata komercyjnego doświadczenia w wytwarzaniu oprogramowania z czego '
+                            'dwa lata w języku Java. Większość czasu spędziłem nad pracą w dużych projektach '
+                            'dotyczących ceł i podatków dla duńskiego sektora publicznego. Posiadałem szeroki zakres '
+                            'obowiązków, od tworzenia testów automatycznych, implementacji nowych funkcjonalności i '
+                            'wdrożeń po przygotowywanie kontraktów. Ukończyłem studia magisterskie z informatyki na '
+                            'wydziale elektroniki i technik informacyjnych politechniki warszawskiej.',
+        'projects': 'Projekty',
+        'cv': 'CV',
+        'contact': 'Kontakt',
+        'my_projects': 'Lista moich projektów.',
+        'my_cv': 'Moje CV.',
+        'my_contact': 'Możesz się ze mną skontaktować przez e-mail: mpiotro4@outlook.com.',
+    },
+    'en': {
+        'title': 'Marcin Piotrowski - homepage',
+        'about_me': 'About Me',
+        'about_me_content': "I have over three years of commercial experience in software development, including two "
+                            "years in Java. I spent most of my time working on large projects related to customs and "
+                            "taxes for the Danish public sector. I had a wide range of responsibilities, "
+                            "from creating automated tests and implementing new features to deployments and preparing "
+                            "contracts. I hold a master's degree in computer science from the Faculty of Electronics "
+                            "and Information Technology at the Warsaw University of Technology.",
+        'projects': 'Projects',
+        'cv': 'CV',
+        'contact': 'Contact',
+        'my_projects': 'List of my projects.',
+        'my_cv': 'My CV.',
+        'my_contact': 'You can contact me via email: mpiotro4@outlook.com.',
+    }
+}
+
+
+@app.route('/')
+def home():
+    lang = session.get('lang', 'en')  # Domyślnie język polski
+    return render_template('index.html', lang=lang, translations=translations[lang])
+
+@app.route('/set_language/<language>')
+def set_language(language):
+    session['lang'] = language
+    return redirect(url_for('home'))
+
+@app.route('/projekty')
+def projekty():
+    lang = session.get('lang', 'pl')
+    return render_template('projects.html', lang=lang, translations=translations[lang])
+
+@app.route('/cv')
+def cv():
+    lang = session.get('lang', 'pl')
+    return render_template('cv.html', lang=lang, translations=translations[lang])
+
+@app.route('/kontakt')
+def kontakt():
+    lang = session.get('lang', 'pl')
+    return render_template('contact.html', lang=lang, translations=translations[lang])
+
+if __name__ == '__main__':
+    app.run(debug=True)
